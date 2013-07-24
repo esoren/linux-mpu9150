c4dd7d78cde0202e891b475c1f36d870c3f9002e
esoren <ravix475@gmail.com>
2013-07-23 19:49:04 -0700

    fflush

--- imu.c
+++ imu.c
@@ -191,6 +191,7 @@ void read_loop(unsigned int sample_rate)
 			print_fused_quaternions(&mpu);
 			print_calibrated_accel(&mpu);
 			print_calibrated_mag(&mpu);
+			fflush(stdout);
 		}
 
 		linux_delay_ms(loop_delay);
@@ -206,7 +207,7 @@ void print_fused_euler_angles(mpudata_t *mpu)
 			mpu->fusedEuler[VEC3_Y] * RAD_TO_DEGREE, 
 			mpu->fusedEuler[VEC3_Z] * RAD_TO_DEGREE);
 
-	fflush(stdout);
+	
 }
 
 void print_fused_quaternions(mpudata_t *mpu)
@@ -217,7 +218,7 @@ void print_fused_quaternions(mpudata_t *mpu)
 			mpu->fusedQuat[QUAT_Y],
 			mpu->fusedQuat[QUAT_Z]);
 
-	fflush(stdout);
+	
 }
 
 void print_calibrated_accel(mpudata_t *mpu)
@@ -227,7 +228,7 @@ void print_calibrated_accel(mpudata_t *mpu)
 			mpu->calibratedAccel[VEC3_Y], 
 			mpu->calibratedAccel[VEC3_Z]);
 
-	fflush(stdout);
+	
 }
 
 void print_calibrated_mag(mpudata_t *mpu)
@@ -237,7 +238,7 @@ void print_calibrated_mag(mpudata_t *mpu)
 			mpu->calibratedMag[VEC3_Y], 
 			mpu->calibratedMag[VEC3_Z]);
 
-	fflush(stdout);
+	
 }
 
 int set_cal(int mag, char *cal_file)
